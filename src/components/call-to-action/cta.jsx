65f6c8682e7ddf91f2321c54ea4d3b86bad9682c
David Sobral <david.sobral@valtech.com>
2023-02-14 15:12:13 -0300

    CTA component

---
+++ src/components/call-to-action/cta.jsx
@@ -0,0 +1,36 @@
+import hero from '../../assets/hero.png';
+
+export default function CTA() {
+  return (
+    <main className='hero'>
+        <article className='hero'>
+          <section className='heroContent'>
+            <h1 className='title'>
+              Little Lemon
+            </h1>
+
+            <h2 className='subtitle'>
+              Chicago
+            </h2>
+
+            <p className='description'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+
+            <section className='button'>
+              <button href='https://www.google.com' target='_blank'>
+                Reserve a table
+              </button>
+            </section>
+          </section>
+
+          <section className='heroImage'>
+            <img
+              src={hero}
+              alt='Hero'
+            />
+          </section>
+        </article>
+      </main>
+  );
+};
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,5 +1,4 @@
 import './main.css';
-import hero from '../../assets/hero.png';
 import salad from '../../assets/greek-salad.png';
 import bruchetta from '../../assets/bruchetta.png';
 import dessert from '../../assets/lemon-dessert.png';
@@ -8,40 +7,12 @@ import avatar from '../../assets/avatar.png';
 import images from '../../assets/images.png';
 
 import Rating from '../../components/star-rating/star-rating';
+import Hero from '../../components/call-to-action/cta';
 
 export default function Main() {
   return (
     <>
-      <main className='hero'>
-        <article className='hero'>
-          <section className='heroContent'>
-            <h1 className='title'>
-              Little Lemon
-            </h1>
-
-            <h2 className='subtitle'>
-              Chicago
-            </h2>
-
-            <p className='description'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </p>
-
-            <section className='button'>
-              <button href='https://www.google.com' target='_blank'>
-                Reserve a table
-              </button>
-            </section>
-          </section>
-
-          <section className='heroImage'>
-            <img
-              src={hero}
-              alt='Hero'
-            />
-          </section>
-        </article>
-      </main>
+      <Hero />
 
       <main className='specials'>
         <article className='specials'>
