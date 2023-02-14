ddb3c60a82a27ee32901fb0359e7bb0177441cba
David Sobral <david.sobral@valtech.com>
2023-02-14 15:45:10 -0300

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Homepage from './components/homepage/homepage';
+import Booking from './components/bookingPage/bookingPage';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Homepage />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          index
+          element={
+            <Homepage />
+          }
+        />
+
+        <Route
+          path='/reserve-a-table'
+          element={
+            <Booking />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
