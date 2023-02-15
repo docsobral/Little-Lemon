773f60b65c2b64d6e709282675b9614544c45484
David Sobral <david.sobral@valtech.com>
2023-02-15 13:07:02 -0300

    Booking page booking module and styling

--- src/components/bookingPage/bookingPage.jsx
+++ src/components/bookingPage/bookingPage.jsx
@@ -1,7 +1,18 @@
+import './bookingPage.css';
+import Header from '../header/header';
+
 export default function BookingPage() {
   return (
     <>
-    
+      <Header />
+
+      <main>
+        <article className='booking'>
+          <section className='inner'>
+            <h1>Reserve a table:</h1>
+          </section>
+        </article>
+      </main>
     </>
   )
 }
