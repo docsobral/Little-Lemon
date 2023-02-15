efa1443987c363472a7a8f4d659f81aa1043b630
David Sobral <david.sobral@valtech.com>
2023-02-15 12:55:00 -0300

    Added routing and context

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import Homepage from './components/homepage/homepage';
 import Booking from './components/bookingPage/bookingPage';
+import AppContext from './components/context/context';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          index
-          element={
-            <Homepage />
-          }
-        />
+  const [booking, setBooking] = useState(false);
 
-        <Route
-          path='/reserve-a-table'
-          element={
-            <Booking />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+  return (
+    <AppContext.Provider value={{ booking, setBooking }}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            element={
+              <Homepage />
+            }
+          />
+
+          <Route
+            path='/reserve-a-table'
+            element={
+              <Booking />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
---
+++ src/components/context/context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const AppContext = createContext({
+  booking: false,
+});
+
+export default AppContext;
