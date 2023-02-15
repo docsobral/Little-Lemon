1a5db2817c68ee5149241787c71a9ac4dd90d514
David Sobral <david.sobral@valtech.com>
2023-02-15 09:34:01 -0300

    Added links to sections of the homepage to the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,32 +4,33 @@ import image from '../../assets/image.png'
 export default function Footer() {
   return (
     <footer>
-      <article className="footer">
-        <section className="image">
+      <article className='footer'>
+        <section className='image'>
           <img
             src={image}
+            alt='Footer img'
           />
         </section>
 
-        <section className="navs">
-          <section className="nav">
+        <section className='navs'>
+          <section className='nav'>
             <b>Navigation</b>
 
-            <nav className="one">
+            <nav className='one'>
               <li>&nbsp;</li>
-              <li>Home</li>
-              <li>About</li>
-              <li>Menu</li>
-              <li>Reservations</li>
-              <li>Order Online</li>
-              <li>Login</li>
+              <li><a href='localhost:3000' style={{color: 'white', textDecoration: 'none'}}>Home</a></li>
+              <li><a href='#about' style={{color: 'white', textDecoration: 'none'}}>About</a></li>
+              <li><a href='#specials' style={{color: 'white', textDecoration: 'none'}}>Menu</a></li>
+              <li><a href='#hero' style={{color: 'white', textDecoration: 'none'}}>Reservations</a></li>
+              <li><a href='#specials' style={{color: 'white', textDecoration: 'none'}}>Order Online</a></li>
+              <li><a href='localhost:3000' style={{color: 'white', textDecoration: 'none'}}>Login</a></li>
             </nav>
           </section>
 
-          <section className="nav">
+          <section className='nav'>
             <b>Contact</b>
 
-            <nav className="two">
+            <nav className='two'>
               <li>&nbsp;</li>
               <li>Address</li>
               <li>Phone number</li>
@@ -37,10 +38,10 @@ export default function Footer() {
             </nav>
           </section>
 
-          <section className="nav">
+          <section className='nav'>
             <b>Social Media Links</b>
 
-            <nav className="three">
+            <nav className='three'>
               <li>&nbsp;</li>
               <li>Address</li>
               <li>Phone number</li>
