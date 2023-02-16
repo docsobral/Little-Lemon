e4389c4764dea2dd62bf31cd31a59a4e6d6c49f8
David Sobral <david.sobral@valtech.com>
2023-02-16 09:51:42 -0300

    Nav now successfully redirects to home

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,13 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Nav() {
+  const navigate = useNavigate();
+
+  function goHome() {
+    navigate('/');
+  };
+
   return (
     <section className='headerNav'>
       <nav>
-        <li><a href='localhost:3000' style={{color: 'black', textDecoration: 'none'}}>Home</a></li>
+        <li><a onClick={goHome} style={{color: 'black', textDecoration: 'none'}}>Home</a></li>
         <li><a href='#about' style={{color: 'black', textDecoration: 'none'}}>About</a></li>
         <li><a href='#specials' style={{color: 'black', textDecoration: 'none'}}>Menu</a></li>
         <li><a href='#hero' style={{color: 'black', textDecoration: 'none'}}>Reservations</a></li>
         <li><a href='#specials' style={{color: 'black', textDecoration: 'none'}}>Order Online</a></li>
-        <li><a href='localhost:3000' style={{color: 'black', textDecoration: 'none'}}>Login</a></li>
+        <li><a onClick={goHome} style={{color: 'black', textDecoration: 'none'}}>Login</a></li>
       </nav>
     </section>
   );
