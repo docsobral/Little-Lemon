08f782321e8414ad9c37311381d39fabf8a0da25
David Sobral <david.sobral@valtech.com>
2023-02-16 12:28:14 -0300

    Booking times now use useReducer instead of useState

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,6 +1,6 @@
 import './main.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 
 import Homepage from '../homepage/homepage';
 import Booking from '../bookingPage/bookingPage';
@@ -12,10 +12,22 @@ export default function Main() {
   const [time, setTime] = useState(undefined);
   const [number, setNumber] = useState(2);
   const [occasion, setOccasion] = useState(undefined);
-  const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+
+  // const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+
+  const initializeTimes = () => {
+    const times = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+    return times.map((time, index) => <option key={index}>{time}</option>);
+  };
+  const updateTimes = (state, action) => {
+    return state =  initializeTimes();
+  };
+
+  const [availableTimes, setAvailableTimes] = useReducer(updateTimes, initializeTimes());
+
 
   useEffect(() => {
-    setAvailableTimes(availableTimes.map((time, index) => <option key={index}>{time}</option>));
+    updateTimes();
     console.log(availableTimes);
   }, []);
 
@@ -43,5 +55,5 @@ export default function Main() {
         />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
