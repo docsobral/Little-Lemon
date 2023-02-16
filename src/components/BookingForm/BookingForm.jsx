1e6d4a4b31e9f0cbb8dc24886985883a613305d6
David Sobral <david.sobral@valtech.com>
2023-02-16 10:37:36 -0300

    BookingForm logic

--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -1,42 +1,47 @@
 import './BookingForm.css';
-import { Formik, Field, Form } from 'formik';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const today = new Date().toLocaleDateString()
 
 export default function BookingForm() {
-  const [field1, setField1] = useState()
+  const [date, setDate] = useState(today);
+  const [time, setTime] = useState(undefined);
+  const [number, setNumber] = useState(2);
+  const [occasion, setOccasion] = useState(undefined);
+
+  const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'])
+
+  useEffect(() => {
+    setAvailableTimes(availableTimes.map((time, index) => <option key={index}>{time}</option>));
+    console.log(availableTimes);
+  }, []);
+
   return (
-    <Formik>
-      <Form>
-        <section className='date'>
-          <label htmlFor='res-date'>Choose date:</label>
-          <Field type='date' id='res-date' name='res-date' placeholder={new Date().toLocaleDateString()} />
-        </section>
+    <form>
+      <section className='date'>
+        <label htmlFor='res-date'>Choose date:</label>
+        <input type='date' id='res-date' name='res-date' placeholder={date} onChange={setDate} />
+      </section>
 
-        <section className='time'>
-          <label htmlFor='res-time'>Choose time:</label>
-          <select id='res-time' name='res-time'>
-            <option>17:00</option>
-            <option>18:00</option>
-            <option>19:00</option>
-            <option>20:00</option>
-            <option>21:00</option>
-            <option>22:00</option>
-          </select>
-        </section>
+      <section className='time'>
+        <label htmlFor='res-time'>Choose time:</label>
+        <select id='res-time' name='res-time' onChange={setTime}>
+          {availableTimes}
+        </select>
+      </section>
 
-        <section className='guest-number'>
-          <label htmlFor='guests'>Number of guests:</label>
-          <Field type='number' min='0' max='5' id='guests' name='guests' placeholder={2} />
-        </section>
+      <section className='guest-number'>
+        <label htmlFor='guests'>Number of guests:</label>
+        <input type='number' min='0' max='5' id='guests' name='guests' placeholder={number} onChange={setNumber} />
+      </section>
 
-        <section className='occasion'>
-          <label htmlFor='occasion'>Occasion:</label>
-          <select id='occasion' name='occasion'>
-            <option>Birthday</option>
-            <option>Anniversary</option>
-          </select>
-        </section>
-      </Form>
-    </Formik>
+      <section className='occasion'>
+        <label htmlFor='occasion'>Occasion:</label>
+        <select id='occasion' name='occasion' onChange={setOccasion}>
+          <option>Birthday</option>
+          <option>Anniversary</option>
+        </select>
+      </section>
+    </form>
   );
 };
